c1115df0817979c257cb0c38d0a202593a35ebe8
Mony JR <monyjr@Monys-iMac.local>
2024-03-29 15:07:00 +0700

    git

---
+++ app/Container/Cato.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { MdNotifications } from "react-icons/md";
+
+const Cato = () => {
+  return (
+    <div className='flex justify-center mt-2'>
+    <div className='Cato w-11/12 h-40 rounded-2xl flex-col justify-between flex  items-center'>
+        <div className="location flex flex-col justify-between w-full h-full items-start">
+              <select className='text-red-500 mt-2 ml-2'>
+                <option selected>Pnhom phenh</option>
+                <option>Siem Reap </option>
+            </select>  
+            <label >This job</label>
+            <span className='absolute right-7
+             mt-5 w-20 h-20 text-3xl
+              bg-red-500 rounded-full 
+              flex justify-center
+              items-center
+              
+              '><MdNotifications /></span>
+        </div>
+        <div>
+            <input className='p-2 mb-3 rounded-2xl'/>
+        </div>
+      
+    </div>
+     </div>
+  )
+}
+
+export default Cato
---
+++ app/Header/Mobile/Menu.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { IoMdHome } from "react-icons/io";
+import "../Menu.css"
+import { FaFile } from "react-icons/fa";
+
+import { FaCircleUser } from "react-icons/fa6";
+import { FaClipboardCheck } from "react-icons/fa";
+
+
+const Menu = () => {
+  return (
+    <div className='flex sm:flex-col justify-between 
+    items-center text-3xl 
+    fixed h-20 w-full 
+    bottom-0 pl-5 pr-5 
+    pt-2 pb-2 menu '
+    
+    >
+      <span className='text-slate-950'><IoMdHome /></span>
+      <span className='text-slate-950'><FaFile /></span>
+      <span className='text-slate-950'><FaClipboardCheck /></span>
+      <span className='text-slate-950'><FaCircleUser /></span>
+    </div>
+  )
+}
+
+export default Menu
