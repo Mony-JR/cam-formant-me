d9433ed3a92ee510f0b19b4ea413775214a93bcb
Moony-Jr-window <sreyloth97a@gmail.com>
2024-04-23 11:20:19 +0700

    search

--- app/Search/Search.tsx
+++ app/Search/Search.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+"use client"
+export default function Search() {
 
-const Search = () => {
+  const se=[
+    {txt:'IT support'},
+    {txt:'Developer'},
+    {txt:'Information Technology'},
+    {txt:'Front-end'},
+    {txt:'Full stact Dev'}
+
+  ]
+
   return (
-    <div>
-      
+
+    <div className="flex flex-wrap justify-center items-start gap-3">
+      {se.map((p)=>(
+        <span className="p-2 bg-red-300 rounded-xl">{p.txt}</span>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Search
--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -5,13 +5,17 @@ import { TbMenuDeep } from "react-icons/tb";
 import { IoMdArrowBack } from "react-icons/io";
 import Link from 'next/link';
 import Popup from '../Container/Popup';
+import Search from './Search';
 
 
 const Searching = () => {
 
   const [pop1, setPop] = useState<boolean>(true);
+  const [s1,sets1]=useState<string>('')
 
   return (
+    <>
+    <h1>{s1}</h1>
     <div className='flex justify-center items-center h-[100px]'>
       <div>
         <Link href={"/"}><span className='text-3xl'><IoMdArrowBack /></span></Link>
@@ -21,7 +25,9 @@ const Searching = () => {
          justify-start items-center 
         w-full h-full'>
 
-        <input autoFocus placeholder='hello' type='text' className='p-4 ml-4
+        <input autoFocus placeholder='hello' type='text' value={s1} onChange={e=> sets1(e.target.value)}
+        
+        className='p-4 ml-4
            placeholder:text-gray-400
             rounded-2xl text-black input r
            elative w-11/12 outline-none pl-20 shadow-md' />
@@ -37,6 +43,9 @@ const Searching = () => {
 
       <Popup jj={setPop} up={pop1} />
     </div>
+    <Search/>
+
+    </>
   )
 }
 
