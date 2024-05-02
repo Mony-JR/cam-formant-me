2a4c8850ce039a13eaf375c508d7a8a99c78e49f
Mony JR <monyjr@Monys-iMac.local>
2024-05-02 10:08:50 +0700

    work

--- app/Search/Search.tsx
+++ app/Search/Search.tsx
@@ -1,50 +1,44 @@
-
-interface pp{
-  S1:string
-  S2:string
+interface Pp {
+  S1: string;
 }
 
-  interface Show{
-    txt:string,
-    po?:string
-  }
+interface Show {
+  txt: string;
+  po?: string;
+}
 
-const Search:React.FC<pp>=({S1,S2})=> {
+const SearchComponent: React.FC<Pp> = ({ S1 }) => {
 
-  const se:Show[]=[
+  const se: Show[] = [
     {
-      txt:'IT support',
-      po:'Staff'
+      txt: 'IT support',
+      po: 'Staff'
     },
     {
-      txt:'IT support',
-      po:'intern ship '
+      txt: 'IT support',
+      po: 'intern ship '
     },
-    {txt:'IT'},
-    {txt:'Developer'},
-    {txt:'Information Technology'},
-    {txt:'Front-end'},
-    {txt:'Full stact Dev'}
-
+    { txt: 'IT' },
+    { txt: 'Developer' },
+    { txt: 'Information Technology' },
+    { txt: 'Front-end' },
+    { txt: 'Full stact Dev' }
   ]
 
-
-  const fil_search=se.filter((ss)=>ss.txt.toUpperCase().includes(S1.toUpperCase()))
-
+  const fil_search = se.filter((ss) => ss.txt.toUpperCase().includes(S1.toUpperCase()))
 
   return (
-
     <div className="flex flex-col gap-10">
       <div className="flex flex-wrap justify-center items-start gap-3">
-      {fil_search.map((p,index)=>(
-        <span key={index} className="p-2 bg-red-300 rounded-xl">{p.txt}</span>
-      ))}
+        {fil_search.map((p, index) => (
+          <span key={index} className="p-2 bg-red-300 rounded-xl">{p.txt}</span>
+        ))}
       </div>
       <div className=" flex flex-col gap-1 justify-center items-center ">
         {
-          fil_search.map((p,index)=>(
+          fil_search.map((p, index) => (
             <div key={index} className="w-[95%] h-[300px] bg-red-500 ">
-              
+
             </div>
           ))
         }
@@ -52,4 +46,5 @@ const Search:React.FC<pp>=({S1,S2})=> {
     </div>
   );
 }
-export default Search
+
+export default SearchComponent;
--- app/Search/page.tsx
+++ app/Search/page.tsx
@@ -1,51 +1,49 @@
 "use client"
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { IoSearch } from "react-icons/io5";
 import { TbMenuDeep } from "react-icons/tb";
 import { IoMdArrowBack } from "react-icons/io";
 import Link from 'next/link';
 import Popup from '../Container/Popup';
-import Search from './Search';
-
+import SearchComponent from './Search'; // Renamed the component to SearchComponent
 
 const Searching = () => {
 
   const [pop1, setPop] = useState<boolean>(true);
-  const [s1,sets1]=useState<string>('')
+  const [s1, sets1] = useState<string>('');
 
   return (
     <>
-    <div className='flex justify-center items-center h-[100px]'>
-      <div>
-        <Link href={"/"}><span className='text-3xl'><IoMdArrowBack /></span></Link>
-      </div>
-      <div className='flex
-        gap-3
-         justify-start items-center 
-        w-full h-full'>
+      <div className='flex justify-center items-center h-[100px]'>
+        <div>
+          <Link href={"/"}><span className='text-3xl'><IoMdArrowBack /></span></Link>
+        </div>
+        <div className='flex
+          gap-3
+          justify-start items-center 
+          w-full h-full'>
 
-        <input autoFocus placeholder='hello' type='text' value={s1} onChange={e=> sets1(e.target.value)}
-        
-        className='p-4 ml-4
-           placeholder:text-gray-400
+          <input autoFocus placeholder='hello' type='text' value={s1} onChange={e => sets1(e.target.value)}
+
+          className='p-4 ml-4
+            placeholder:text-gray-400
             rounded-2xl text-black input r
-           elative w-11/12 outline-none pl-20 shadow-md' />
+            elative w-11/12 outline-none pl-20 shadow-md' />
 
-        <span onClick={() => setPop(!pop1)} className='text-xl p-2 
-          absolute right-10'><TbMenuDeep /></span>
+          <span onClick={() => setPop(!pop1)} className='text-xl p-2 
+            absolute right-10'><TbMenuDeep /></span>
 
-        <button className='text-xl 
-          flex items-center justify-center
-           left-10 btn-se p-4 Cato 
-           rounded-full text-white pointer-events-none '><IoSearch /></button>
+          <button className='text-xl 
+            flex items-center justify-center
+            left-10 btn-se p-4 Cato 
+            rounded-full text-white pointer-events-none '><IoSearch /></button>
+        </div>
+
+        <Popup jj={setPop} up={pop1} />
       </div>
-
-      <Popup jj={setPop} up={pop1} />
-    </div>
-    <Search S={sets1} S1={s1}/>
-
+      <SearchComponent S1={s1} />
     </>
   )
 }
 
-export default Searching
+export default Searching;
